7063e3ef591aa5bbb741f15aa62190e733f9042f
Paul Thompson <pmt10@case.edu>
2024-07-09 11:08:42 -0400

    video decoder tests added. added one keyframe ground truth

--- tests/video-decoder-tests/test_decoder.cpp
+++ tests/video-decoder-tests/test_decoder.cpp
@@ -34,31 +34,139 @@ size_t calculate_pixel_difference(const std::vector<uint8_t>& original, const st
         }
     }
     std::cout << "Number of different elements: " << diff_count << std::endl;
+
+    if (diff_count > 0) {
+        save_decoded_image_as_binary(decoded, "data/decoded_image.bin");
+    }
     return diff_count;
 }
 
+static const int tolerance = 5;
+static auto frame_0 = load_img("data/frame_0.bin");
+static auto frame_100 = load_img("data/frame_100.bin");
+static auto frame_200 = load_img("data/frame_200.bin");
+static auto frame_300 = load_img("data/frame_300.bin");
+static auto frame_400 = load_img("data/frame_400.bin");
+static auto frame_500 = load_img("data/frame_500.bin"); //keyframe
+
+static std::string video_filename = "data/test_each_frame_number.mp4";
+
 TEST_CASE("VideoDecoder object creation", "[ffmpeg_wrapper]") {
-    SECTION("Create a VideoDecoder object") {
 
-        std::string video_filename = "data/test_each_frame_number.mp4";
 
-        ffmpeg_wrapper::VideoDecoder decoder;
-        decoder.createMedia(video_filename);
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
 
-        CHECK(decoder.getHeight() == 480);
-        CHECK(decoder.getWidth() == 640);
+    CHECK(decoder.getHeight() == 480);
+    CHECK(decoder.getWidth() == 640);
+}
 
-        static auto frame_0 = load_img("data/frame_0.bin");
+TEST_CASE("VideoDecoder get keyframes", "[ffmpeg_wrapper]") {
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
 
-        auto frame_0_decoded = decoder.getFrame(0);
-
-        save_decoded_image_as_binary(frame_0_decoded, "data/decoded_frame_0.bin");
-
-        CHECK(frame_0.size() == frame_0_decoded.size());
-
-        int tolerance = 5;
-        size_t diff_count = calculate_pixel_difference(frame_0, frame_0_decoded, tolerance);
-        CHECK(diff_count == 0);
-
+    //Print keyframes
+    auto keyframes = decoder.getKeyFrames();
+    std::cout << "Keyframes: " << std::endl;
+    for (auto keyframe: keyframes) {
+        std::cout << keyframe << std::endl;
     }
+
+    CHECK(keyframes.size() == 5);
+}
+
+TEST_CASE("VideoDecoder get frame", "[ffmpeg_wrapper]") {
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+    auto frame_0_decoded = decoder.getFrame(0);
+
+    CHECK(frame_0.size() == frame_0_decoded.size());
+
+    size_t diff_count = calculate_pixel_difference(frame_0, frame_0_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+
+
+}
+
+TEST_CASE("VideoDecoder get frame by frame", "[ffmpeg_wrapper]") {
+
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+
+    for (int i = 0; i < 200; ++i) {
+        auto frame_decoded = decoder.getFrame(i);
+    }
+
+    auto frame_200_decoded = decoder.getFrame(200);
+
+    size_t diff_count = calculate_pixel_difference(frame_200, frame_200_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+}
+
+TEST_CASE("VideoDecoder move backward", "[ffmpeg_wrapper]") {
+
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+
+    decoder.getFrame(0); // Needs to decode first frame to not segfault?
+
+    auto frame_200_decoded = decoder.getFrame(200);
+
+    size_t diff_count = calculate_pixel_difference(frame_200, frame_200_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+    // move back to frame 0 and check if the decoded frame is the same as frame 0
+    auto frame_0_decoded = decoder.getFrame(0);
+    diff_count = calculate_pixel_difference(frame_0, frame_0_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+}
+
+TEST_CASE("VideoDecoder move backward frame by frame", "[ffmpeg_wrapper]") {
+
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+    decoder.getFrame(0);
+
+    auto frame_200_decoded = decoder.getFrame(200);
+
+    size_t diff_count = calculate_pixel_difference(frame_200, frame_200_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+    // move back to frame 0 and check if the decoded frame is the same as frame 0
+    for (int i = 200; i >= 0; --i) {
+        auto frame_decoded = decoder.getFrame(i);
+    }
+
+    auto frame_0_decoded = decoder.getFrame(0);
+    diff_count = calculate_pixel_difference(frame_0, frame_0_decoded, tolerance);
+    CHECK(diff_count == 0);
+
+}
+
+TEST_CASE("Determine nearest keyframe","[ffmpeg_wrapper]")
+{
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+    decoder.getFrame(0);
+
+    CHECK(decoder.nearest_iframe(0) == 0);
+    CHECK(decoder.nearest_iframe(1) == 0);
+    CHECK(decoder.nearest_iframe(300) == 250);
+    CHECK(decoder.nearest_iframe(500) == 500);
+}
+
+TEST_CASE("Seek to keyframes","[ffmpeg_wrapper]")
+{
+    ffmpeg_wrapper::VideoDecoder decoder;
+    decoder.createMedia(video_filename);
+    decoder.getFrame(0);
+
+    //decoder.getFrame(250);
+
+    //auto frame_decoded_500 = decoder.getFrame(500);
+    //size_t diff_count = calculate_pixel_difference(frame_500, frame_decoded_500, tolerance);
+    //CHECK(diff_count == 0);
 }
